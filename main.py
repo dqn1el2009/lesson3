4e2a95f1e50b3e87c92d5e32f983cdc071de3807
dqn1el2009 <daniel.botnary@gmail.com>
2023-09-24 11:04:31 +0300

    main

---
+++ main.py
@@ -0,0 +1,72 @@
+#array = ["Math", "Info", "Physics", "General culture"]
+#int_array = [1, 2, 3, 4, 5, 6]
+#float_array = [1.3, -2, 3.14, 4.7, 5.002, 6.1214]
+#bool_array = [True, False, True, False]
+
+#array.append("English")
+
+#array = sorted(array)
+#array.sort()
+#int_array.sort(reverse=True)
+
+#print(int_array)
+#print(sorted(int_array))
+
+
+
+
+#list = []
+#list_lenght = int(input("Enter List Lenght\n"))
+
+#for i in range(0, list_lenght):
+  #  element = int(input("Enter Element\n"))
+ #   list.append(element)
+
+#print(list)
+#list.sort()
+#print(list.sort())
+#list.sort(reverse=True)
+#print(list)
+
+#if 56 in list:
+    #print(True)
+#else:
+    #print(False)
+
+
+
+#list = [12, 56, 2465, 547, 245, 876]
+#print(list)
+
+#for i in range(0, len(list)):
+    #print(list[i]) # print list by index
+
+#for e in list:
+    #print(e) # print list by element
+
+
+#tuple = ("A", "B", "C", "D")
+
+#print(tuple + ("E", "Finish"))
+
+
+#int_array = [1, 2, 3, 1, 5, 2, 6 ,7]
+#int_array = set(int_array)
+#set_array = {1, 1, 2, 3, 1, 5, 2, 6 ,7}
+#print(set_array)
+#print(int_array)
+
+
+dex = {"Cod": "Python", "Extend": ".py", "Less": "3"}
+
+print(dex.get("Cod")) # use key to display value with the
+
+dex.update({"Less": 3}) # update value where key is less
+
+print(dex) # print dex
+
+for i in dex:
+    print(i) # display key
+    print(dex.get(i)) #display value with key(i)
+
+diferenta dintre array si tuple
